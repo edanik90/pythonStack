e36203ae4ebae9eabfbaf9d1fd188bb8ddc26a20
Dan <e.danik90@gmail.com>
2020-01-07 12:00:46 -0600

    a few updates

--- python/fundamentals/functionsIntermediate1.py
+++ python/fundamentals/functionsIntermediate1.py
@@ -4,9 +4,8 @@ def randInt(min = 0, max = 100):
         temp = min
         min = max
         max = temp
-    num = random.random() * (max - min) + min
-    return round(num)
-# print(randInt()) # should print a random integer between 0 to 100
-# print(randInt(max=50)) 	    # should print a random integer between 0 to 50
-# print(randInt(min=50)) 	    # should print a random integer between 50 to 100
+    return round(random.random() * (max - min) + min)
+print(randInt()) # should print a random integer between 0 to 100
+print(randInt(max=50)) 	    # should print a random integer between 0 to 50
+print(randInt(min=50)) 	    # should print a random integer between 50 to 100
 print(randInt(min=50, max=-4))    # should print a random integer between 50 and 500
