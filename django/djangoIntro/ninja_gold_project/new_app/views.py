40c040598001fb74480b49780dbfb718033707c4
Dan <e.danik90@gmail.com>
2020-01-13 21:18:33 -0600

    Ninja Gold updated

--- django/djangoIntro/ninja_gold_project/new_app/views.py
+++ django/djangoIntro/ninja_gold_project/new_app/views.py
@@ -5,34 +5,36 @@ import random
 def index(request):
     if not 'gold_amount' in request.session:
         request.session['gold_amount'] = 0
-    if not 'activity' in request.session:
-        request.session['activity'] = ""
+    if request.session['gold_amount'] < 0:
+        restart_game(request)
+    if not 'log' in request.session:
+        request.session['log'] = ""
     return render(request, "index.html")
 
 def process_money(request):
     if request.POST["location"] == "farm":
         temp = random.randint(0,10)
         request.session['gold_amount'] += temp
-        request.session['activity'] += f"\nEarned {temp} gold at the farm!"
+        request.session['log'] += f"\nEarned {temp} gold at the farm!"
     if request.POST["location"] == "cave":
         temp = random.randint(5,10)
         request.session['gold_amount'] += temp
-        request.session['activity'] += f"\nEarned {temp} gold at the cave!"
+        request.session['log'] += f"\nEarned {temp} gold at the cave!"
     if request.POST["location"] == "house":
         temp = random.randint(2,5)
         request.session['gold_amount'] += temp
-        request.session['activity'] += f"\nEarned {temp} gold at the house!"
+        request.session['log'] += f"\nEarned {temp} gold at the house!"
     if request.POST["location"] == "casino":
         temp = random.randint(-50,50)
         request.session['gold_amount'] += temp
         if temp > 0:
-            request.session['activity'] += f"\nEntered casino and won {temp}!!!"
+            request.session['log'] += f"\nEntered casino and won {temp}!!!"
         else:
-            request.session['activity'] += f"\nEntered casino and lost {abs(temp)}...Ouch..."
-    request.session['activity'] +=" (" + strftime("%B %d, %Y %H:%M %p", gmtime()) + ")"
+            request.session['log'] += f"\nEntered casino and lost {abs(temp)}...Ouch..."
+    request.session['log'] +=" (" + strftime("%B %d, %Y %H:%M %p", gmtime()) + ")"
     return redirect("/")
 
 def restart_game(request):
     request.session['gold_amount'] = 0
-    request.session['activity'] = ""
+    request.session['log'] = ""
     return redirect("/")
