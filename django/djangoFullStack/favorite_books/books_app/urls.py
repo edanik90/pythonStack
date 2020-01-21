59b48d1cdba6a9bdc897d4b6817c2348568cd8c4
Dan <e.danik90@gmail.com>
2020-01-21 14:14:02 -0600

    Favorite books added

--- django/djangoFullStack/favorite_books/books_app/urls.py
+++ django/djangoFullStack/favorite_books/books_app/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.main),
+    path('my_favorite_books', views.my_favorite_books),
     path('add_new_book', views.add_new_book),
     path('<int:book_id>', views.book_info),
+    path('edit/<int:book_id>',views.edit_book),
+    path('<int:book_id>/unfavorite', views.unfavorite),
+    path('<int:book_id>/add_favorite', views.add_to_favorite),
     path('logout', views.logout)
 ]
--- django/djangoFullStack/favorite_books/books_app/views.py
+++ django/djangoFullStack/favorite_books/books_app/views.py
@@ -9,10 +9,17 @@ def main(request):
     }
     return render(request, "books/main.html", context)
 
+def my_favorite_books(request):
+    context = {
+        "books":User.objects.get(id = request.session['user_id']).favorite_books.all()
+    }
+    return render(request, "books/favorite_books.html", context)
+
 def book_info(request, book_id):
     context = {
         "book":Book.objects.get(id = book_id),
-        "users":User.objects.all()
+        "users":User.objects.all(),
+        "user":User.objects.get(id = request.session['user_id'])
     }
     book = Book.objects.get(id = book_id)
     user = User.objects.get(id = request.session['user_id'])
@@ -32,6 +39,29 @@ def add_new_book(request):
         uploaded_by = uploading_user)
     uploading_user.favorite_books.add(new_book)
     return redirect("/books")
+
+def edit_book(request, book_id):
+    book = Book.objects.get(id = book_id)
+    if "update" in request.POST:
+        book.title = request.POST['title']
+        book.description = request.POST['description']
+        book.save()
+        return redirect(f"/books/{book_id}")
+    if "delete" in request.POST:
+        book.delete()
+        return redirect("/books")
+
+def add_to_favorite(request, book_id):
+    book = Book.objects.get(id = book_id)
+    user = User.objects.get(id = request.session['user_id'])
+    user.favorite_books.add(book)
+    return redirect(f"/books/{book_id}")
+
+def unfavorite(request, book_id):
+    book = Book.objects.get(id = book_id)
+    user = User.objects.get(id = request.session['user_id'])
+    user.favorite_books.remove(book)
+    return redirect(f"/books/{book_id}")
 
 def logout(request):
     request.session.clear()
